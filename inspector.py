0d8a59b84e30fd1d47256bb10d53af13fc0c35e3
mlt_melt <lmm113@icloud.com>
2023-07-06 15:01:05 +0300

    add some new content types

--- inspector.py
+++ inspector.py
@@ -12,7 +12,9 @@ if os.path.getsize("logs/logs.log") >= 52428800:
 
 logging.basicConfig(level=logging.INFO, filename="logs/logs.log", format="%(asctime)s | %(message)s")
 
-@dp.message_handler(content_types=['audio', 'photo', 'voice', 'video', 'document', 'text', 'location', 'contact', 'sticker', 'poll'])
+@dp.message_handler(content_types=['audio', 'photo', 'voice', 'video', 'document', 'text',
+                                   'location', 'contact', 'sticker', 'poll',
+                                   'animation','game', 'video_note', 'dice', 'venue'])
 async def moderate(message: types.Message):
     user_id = message.from_user.id
 
@@ -51,3 +53,4 @@ async def moderate(message: types.Message):
                 user_id = message.text.split(" ")[1]
                 await bot.promote_chat_member(message.chat.id, user_id)
                 await message.answer(f"User has been unbaned")
+                await message.answer(f'somethi')
